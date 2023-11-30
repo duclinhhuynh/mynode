d9dbda9f31b544887f4f486dd6d596fdedf82b25
linh <linh1862002@gamil.com>
2023-11-30 09:02:43 +0000

    rest_api_login

---
+++ src/services/userServices.js
@@ -0,0 +1,59 @@
+import db from "../models/index";
+import bcrypt from'bcryptjs';
+let handleUserLogin = (email, password) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let userData  = {};
+            let isExit = await checkUserEmail(email)
+            if(isExit){
+                let user = await db.User.findOne({
+                    where: {email : email},
+                    raw: true,
+                });
+                if(user){
+                    let check  = await bcrypt.compareSync(password,  user.password)
+                    if(check){
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        delete user.password;
+                        userData.user = user;
+                    }
+                    else{
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                }else {
+                    userData.errCode = 2;
+                    userData.errMessage = "User is not found"
+                }
+                //    bcrypt.compareSync("not_bacon", hash)//false
+            }else{
+                userData.errCode = 1;
+                userData.errMessage = `Your's Email in't exit`
+            }
+            resolve(userData)
+        } catch (error) {
+            
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: {email : userEmail}
+            })
+            if(user){
+                resolve(true)
+            }else{
+                resolve(false)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
